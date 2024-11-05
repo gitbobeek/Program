dfafa283d68a698e83e768052066d895fb5a853c
gitbobeek <grizzlyenot@gmail.com>
2024-11-05 18:19:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+class Program {
+  public static void main(String[] args) throws IOException {
+    WikiSearch wikiSearch = new WikiSearch();
+    wikiSearch.getRequest();
+
+    try {
+      wikiSearch.makeServerRequest();
+      wikiSearch.showPage();
+    } catch (IOException e) {
+      System.err.println("Ошибка при выполнении запроса: " + e.getMessage());
+    }
+  }
+}
